46251024f47a947ca4bcf1f9525c479efa67cade
yakud <yakud@kit.ec>
2018-02-12 01:57:24 +0200

    Test

--- log-generator-image/run.sh
+++ log-generator-image/run.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 docker run -d \
-    --name=log-generator \
     --log-driver syslog \
     --log-opt syslog-address="tcp://0.0.0.0:603" \
     --log-opt tag="{{.ImageName}}|{{.Name}}|{{.ID}}" \
