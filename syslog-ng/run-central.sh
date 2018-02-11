48488aae86b7e64d4eb8d3da3d9c92d3bd22b2d9
yakud <yakud@kit.ec>
2018-02-11 23:41:19 +0200

    Test

--- syslog-ng/run-central.sh
+++ syslog-ng/run-central.sh
@@ -3,7 +3,7 @@
 docker run -d \
     --name=central \
     --net host \
-    --volume ./syslog-ng/central/syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
-    --volume ./logs/:/tmp/ \
+    --volume $(pwd)/syslog-ng/central/syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf \
+    --volume $(pwd)/logs/:/tmp/ \
     -p 603:603 \
     balabit/syslog-ng:3.7
