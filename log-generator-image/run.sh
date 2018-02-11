afd29aef17994925b188d3fa984fe6c74fbabd0f
yakud <yakud@kit.ec>
2018-02-11 23:43:24 +0200

    Test

--- log-generator-image/run.sh
+++ log-generator-image/run.sh
@@ -4,5 +4,5 @@ docker run -d \
     --name=log-generator \
     --net host \
     --log-driver syslog \
-    --log-opt syslog-address=upd://127.0.0.1:603 \
+    --log-opt syslog-address="upd://127.0.0.1:603" \
     log-generator-image
