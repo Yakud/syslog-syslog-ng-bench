32e7979d3c046cd881a0032698b3a99993c939f7
yakud <yakud@kit.ec>
2018-02-11 22:35:52 +0200

    Test

--- log-generator-image/src/main.go
+++ log-generator-image/src/main.go
@@ -1,28 +1,37 @@
 package main
 
 import (
-	"fmt"
-	"sync"
+	//"os"
+	//"bufio"
+	//"fmt"
+	"os"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
+	//f := bufio.NewWriter(os.Stdout)
+	//for i := 0; i < 10000000; i ++ {
+	//	fmt.Println("yes")
+	//	//f.WriteString("yes\n")
+	//	//f.Flush()
+	//}
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 10000000; i ++ {
-			fmt.Println(i)
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 1000000; i ++ {
-			fmt.Println(i)
-		}
-	}()
-
-	wg.Wait()
+	// create a buffer that has space equal to the page size
+	//buf := make([]byte, 4096)
+	//y := []byte("y\n")
+	//used := 0
+	//for {
+	//	buf[used] = y[0]
+	//	buf[(used)+1] = y[1]
+	//	used += 2
+	//	if used == 4096 {
+	//		break
+	//	}
+	//}
+	// Print out the entire buffer at once - writing direct to
+	// stdout is the fastest way of doing this.
+	// It's the size of a standard linux pagefile.
+	msg := []byte("hello\n")
+	for {
+		os.Stdout.Write(msg)
+	}
 }
