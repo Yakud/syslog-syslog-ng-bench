d0a337dfd9d66c199a5b69469da4f93f74eb276e
yakud <yakud@kit.ec>
2018-02-12 00:22:48 +0200

    Test

--- log-generator-image/src/main.go
+++ log-generator-image/src/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bufio"
 	"os"
+	"fmt"
 )
 
 func main() {
 	msg := []byte("hello\n")
 	f := bufio.NewWriter(os.Stdout)
+	f.Write(msg)
+	f.Flush()
 	for i := 0; i < 10000000; i ++ {
-		f.Write(msg)
-		f.Flush()
+		fmt.Println("hello")
 	}
 }
