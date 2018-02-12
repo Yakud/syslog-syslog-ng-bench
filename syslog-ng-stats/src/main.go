03b9535eeca74640a0cc4aebdd1d2d8bf0da7db2
yakud <yakud@kit.ec>
2018-02-13 00:29:50 +0200

    syslog-ng-stats

--- syslog-ng-stats/src/main.go
+++ syslog-ng-stats/src/main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 	"time"
 	"log"
+	"strings"
+	"strconv"
+	"fmt"
 )
 
+// syslog-ng-ctl stats parser
 func main() {
 	_, err := exec.Command("syslog-ng-ctl","stats", "--reset").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tick := time.Tick(time.Second)
+	data := make(map[string]int)
+
+	secondsInterval := 1
+
+	tick := time.Tick(time.Second * time.Duration(secondsInterval))
 	for ; true; <-tick {
 		out, err := exec.Command("syslog-ng-ctl","stats").Output()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println(string(out))
+		lines := strings.Split(string(out), "\n")
+
+		for _, line := range lines[1:] {
+			parts := strings.Split(line, ";")
+			metric := strings.Join(parts[0:1], ";")
+			currentValue, err := strconv.Atoi(parts[len(parts)-1])
+			if err != nil {
+				currentValue = 0
+			}
+
+			prevValue, ok := data[metric]
+			if !ok {
+				data[metric] = currentValue
+				prevValue = currentValue
+			}
+
+			fmt.Print(metric,"\t",(currentValue - prevValue) / secondsInterval,"/sec\n")
+		}
 	}
 }
